64787c55158d81ae9b5c5bb3c20a01443247d72c
kruplm <73655443+kruplm@users.noreply.github.com>
2024-10-08 12:57:13 +0200

    WIP test for adding extra menu for IG ContentPane's file menu

--- src/shell/dotnet/Shell/WebContentPane.cs
+++ src/shell/dotnet/Shell/WebContentPane.cs
@@ -16,6 +16,19 @@ using System.Threading.Tasks;
 using Infragistics.Windows.DockManager;
 using Infragistics.Windows.DockManager.Events;
 using MorganStanley.ComposeUI.ModuleLoader;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Infragistics.Windows.DockManager.Events;
+using Infragistics.Windows.DockManager;
+using MorganStanley.ComposeUI.ModuleLoader;
+using MorganStanley.ComposeUI.Shell.ImageSource;
+using MorganStanley.ComposeUI.Shell.Utilities;
 
 namespace MorganStanley.ComposeUI.Shell;
 
@@ -33,6 +46,78 @@ internal class WebContentPane : ContentPane
         Closing += Pane_Closing;
         Closed += Pane_Closed;
         WebContent.CloseRequested += WebContent_CloseRequested;
+        OptionsMenuOpening += ContentPane_OptionsMenuOpening;
+    }
+
+    /*
+     
+     <igDock:ContentPane Header="Extra Options"
+                                            OptionsMenuOpening="ContentPane_OptionsMenuOpening">
+                            <!-- The code will add a Save menu item to the pane's options menu so make sure the pane can indicate  if the command is enabled. -->
+                            <igDock:ContentPane.CommandBindings>
+                                <CommandBinding Command="Save"
+                                    CanExecute="CommandBinding_CanExecute"
+                                    Executed="CommandBinding_Executed"/>
+                            </igDock:ContentPane.CommandBindings>
+                        </igDock:ContentPane>
+     */
+
+    private void ContentPane_OptionsMenuOpening(object sender, PaneOptionsMenuOpeningEventArgs e)
+    {
+        ContentPane cp = e.OriginalSource as ContentPane;
+
+        if (e.Items.Count > 0)
+        {
+            Separator sep = new Separator();
+            sep.SetResourceReference(Separator.StyleProperty, XamDockManager.MenuItemSeparatorStyleKey);
+            e.Items.Add(sep);
+        }
+
+
+       
+
+        // the OptionsMenuOpening event is raised when you right click on a
+        // pane's caption or tab item as well as when you click the window
+        // position menu item in the caption and allows you to add/remove
+        // items from the menu.
+        MenuItem mi = new MenuItem();
+
+
+        //// todo replace with channel selector menu and channel selection
+        CommandBinding saveCmdBinding = new CommandBinding(ApplicationCommands.Save, CommandBinding_Executed, CommandBinding_CanExecute);
+        mi.Command = ApplicationCommands.Save; 
+        mi.CommandBindings.Add(saveCmdBinding);
+
+        
+
+
+
+
+        //  Binding bindig = new Binding("CommandBinding_CanExecute");
+        //  BindingOperations.SetBinding(item, comProp, binding);
+
+        // to have the menu items use the same styling as the default
+        // menu items, we'll associate it with the same style that
+        // the dockmanager uses for its menu items
+        mi.SetResourceReference(MenuItem.StyleProperty, XamDockManager.MenuItemStyleKey);
+
+        e.Items.Add(mi);
+    }
+
+    private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+    {
+        // for the purposes of this demonstration, the save command
+        // associated with the binding that raises this event is always
+        // enabled
+        e.CanExecute = true;
+        e.Handled = true;
+    }
+
+    private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+    {
+        // implement save functionality here
+        Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+        dlg.ShowDialog();
     }
 
     private void WebContent_CloseRequested(object? sender, System.EventArgs e)
